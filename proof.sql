84e87ac5e3efbae9b14a3111a74df46b2df8390e
Michael Chen <Michael.l.chen93@gmail.com>
2026-05-18 18:46:09 -0400

    add proof.sql for constraint enforcement

--- proof.sql
+++ proof.sql
@@ -41,3 +41,4 @@ VALUES (1, CURRENT_DATE, 'Bitcoin', 500.00, 'BTC-TEST-001');
 -- Error message from Neon:
 -- ERROR: new row for relation "payment" violates check constraint "payment_payment_method_check" (SQLSTATE 23514)
 
+-- Constraint proof completed.
